753824b7a64035b2d69668ba26ee4ef2af61e291
AlfieRrc0032574 <rc0032574@student.ac.uk>
2019-03-11 14:54:40 +0000

    Add project files.

---
+++ forces/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace forces
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+         double sin(double x)
+        {
+            return (Math.Sin(x * Math.PI / 180.0));
+        }
+
+        double cos(double x)
+        {
+            return (Math.Cos(x * Math.PI / 180.0));
+        }
+
+        double asin(double x )
+        {
+            return (asin(x) * 180 / Math.PI);
+        }
+
+        double acos(double x)
+        {
+            return (acos(x) * 180 / Math.PI);
+        }
+    }
+}
